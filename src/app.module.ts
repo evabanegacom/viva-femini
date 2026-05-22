cdb5ed2606702bb091c75d029b2ce68b5864d3b7
evabanega <pchiedu@chisquares.com>
2026-05-22 17:26:43 +0100

    fixing build error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleFactoryOptions } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { CyclesModule } from './cycles/cycles.module';
 import { SymptomsModule } from './symptoms/symptoms.module';
@@ -9,30 +9,16 @@ import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
-    // Load .env file and make ConfigService available everywhere
     ConfigModule.forRoot({ isGlobal: true }),
 
-    // Connect to MongoDB using env variable; falls back to local dev DB
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: (configService: ConfigService) => ({
-    //     uri: configService.get<string>('MONGODB_URI', 'mongodb://localhost:27017/vivafemme'),
-    //   }),
-    //   inject: [ConfigService],
-    // }),
-
     MongooseModule.forRootAsync({
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService) => {
-    return {
-      uri: configService.get<string>('MONGODB_URI') 
-        || 'mongodb://localhost:27017/vivafemme',
-    };
-  },
-}),
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): MongooseModuleFactoryOptions => ({
+        uri: configService.get<string>('MONGODB_URI', 'mongodb://localhost:27017/vivafemme'),
+      }),
+      inject: [ConfigService],
+    }),
 
-    // Feature modules
     UsersModule,
     CyclesModule,
     SymptomsModule,
